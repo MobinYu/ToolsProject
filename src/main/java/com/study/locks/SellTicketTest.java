70f82477bd6eba0fb251b15bfe8a4908fc4dcbd5
MobinYu <819131005@qq.com>
2019-03-20 09:37:48 +0800

    线程测试类

--- src/main/java/com/study/locks/SellTicketTest.java
+++ src/main/java/com/study/locks/SellTicketTest.java
@@ -6,10 +6,11 @@ public class SellTicketTest {
 //		SellTicket st = new SellTicket();
 		
 //		SellTicket_Sync st = new SellTicket_Sync();
-		SellTicket_Lock st = new SellTicket_Lock();
+//		SellTicket_Lock st = new SellTicket_Lock();
+		SellTicket_Redisson st = new SellTicket_Redisson();
 		
 		//三个线程模拟三个售票窗口
-		for (int i = 0; i < 3; i++) {
+		for (int i = 1; i <= 3; i++) {
 			new Thread(st, "窗口" + i).start();
 		}
 	}
