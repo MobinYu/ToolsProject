da88eff2cae5c88f1e740f476d8c9042bd10feb8
MobinYu <yumabing@163.com>
2019-04-07 20:11:26 +0800

    NIO

--- src/main/java/com/study/io/NIOServer.java
+++ src/main/java/com/study/io/NIOServer.java
@@ -17,6 +17,13 @@ import java.util.concurrent.Executors;
 /**
  * NIO同步非阻塞IO
  * 
+ * 1、Selector: 选择注册到selector的ServerSocketChannel，通过selector.selectedKeys()得到得到就绪Channel的key集合
+ * 2、Channel: 通过selectedKeys得到key的事件，按事件类型处理对应的逻辑
+ * 3、Buffer: 将SocketChannel中的数据都出来
+ * 	3.1：allocate(int)创建不同类型的Buffer
+ * 	3.2：put方法往Buffer中写数据，flip方法将Buffer转为读模式，clear\compact方法清除和整理Buffer（转为写模式）
+ * 	3.3：Buffer三个重要属性，capacity、position、limit
+ * 
  * @author Super Yu
  *
  */
@@ -25,17 +32,15 @@ public class NIOServer {
 	private static CharsetDecoder decoder = charset.newDecoder();
 	
 	public static void main(String[] args) throws IOException {
-		//1、创建一个selector
+		//1、创建一个Selector
 		Selector selector = Selector.open();
 		
 		ServerSocketChannel ssc = ServerSocketChannel.open();
-		int port = 9011;
-		ssc.bind(new InetSocketAddress(port));
+		ssc.bind(new InetSocketAddress(9011));
 		
-		//2、注册到selector
 		ssc.configureBlocking(false); //非阻塞
 		
-		//ssc向selector注册，监听连接到来
+		//2、将要交给Selector检测的SelectableChannel注册进来，监听"连接事件" 
 		ssc.register(selector, SelectionKey.OP_ACCEPT);
 		
 		//连接计数
@@ -48,30 +53,31 @@ public class NIOServer {
 		
 		while (true) {
 			//3、通过Selector来选择就绪的Channel
-			int readyChannelCount = selector.select(); //阻塞等待就绪的事件
+			int readyChannelCount = selector.select(); //阻塞直到有就绪的Channel
 			
-			if (readyChannelCount == 0) {
+			if (readyChannelCount == 0) { //就绪的Channel数量
 				continue;
 			}
 			
-			//得到就绪Channel的key
+			//3、得到就绪SelectionKey的集合
 			Set<SelectionKey> selectionKeys = selector.selectedKeys();
 			
 			Iterator<SelectionKey> keyIterators = selectionKeys.iterator();
 			
+			//******处理就绪集合********
 			while (keyIterators.hasNext()) {
 				SelectionKey key = keyIterators.next();
 				
 				if (key.isAcceptable()) {
 					//a connection was accepted by a ServerSocketChannel.
+					//4、得到就绪通道
 					ServerSocketChannel ssChannle = (ServerSocketChannel) key.channel();
 					//接受连接
 					SocketChannel cc = ssChannle.accept();
 					
-					//请selector帮忙监测数据到来没有
 					//设置非阻塞
 					cc.configureBlocking(false);
-					//向selector注册
+					//5、向selector注册，监听"读就绪事件"，数据到来没有
 					cc.register(selector, SelectionKey.OP_READ, ++connectionCount);
 					
 				} else if (key.isConnectable()) {
@@ -124,14 +130,14 @@ public class NIOServer {
 			SocketChannel sc = (SocketChannel) key.channel();
 			
 			int bfsize = 1024;
-			ByteBuffer rbf = ByteBuffer.allocateDirect(bfsize); //直接分配内存
+			ByteBuffer rbf = ByteBuffer.allocateDirect(bfsize); //直接分配内存（从系统直接分配，allocate从堆中分配）
 			
 			//定义一个更大的buffer
 			ByteBuffer bigBf = null;
 			
 			//读的次数计数
 			int count = 0;
-			while (sc.read(rbf) != -1) {
+			while (sc.read(rbf) != -1) { //sc.read(rbf)读到的字节数
 				count ++;
 				
 				ByteBuffer temp = ByteBuffer.allocateDirect(bfsize * (count + 1));
