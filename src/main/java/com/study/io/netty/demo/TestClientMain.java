2ec17a5f7e1c8758d5afe7b14b9feb885d4f0320
MobinYu <yumabing@163.com>
2020-01-02 22:59:43 +0800

    io

---
+++ src/main/java/com/study/io/netty/demo/TestClientMain.java
@@ -0,0 +1,13 @@
+package com.study.io.netty.demo;
+
+import io.netty.channel.Channel;
+
+public class TestClientMain {
+
+	public static void main(String[] args) {
+		String host = "127.0.0.1";
+	    int port = 9999;
+	    Channel channel = new ImClient().connect(host, port);
+	    channel.writeAndFlush("yinjihuan");
+	}
+}
